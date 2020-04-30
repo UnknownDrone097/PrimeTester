bdffa0ce1ae6bd99cd0b600ef4360b76941a7f0e
UnknownDrone097 <garrettlong097@gmail.com>
2020-04-30 10:46:02 -0400

    Initial commit

---
+++ IsPrime.py
@@ -0,0 +1,19 @@
+def IsPrime(number):
+    divisor = 2
+
+    while (number % divisor) != 0 and divisor < int(number ** .5):
+        if (divisor == 2):
+            divisor += 1
+            continue
+        else:
+            divisor += 2
+    else:
+        if (number % divisor) == 0:
+            return(number, divisor, 'Not prime')
+        else:
+            return(number, 'Prime')
+    divisor = 2
+    power += 1
+
+
+print(IsPrime(float(input('number?'))))
